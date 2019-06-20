5ae49392359422fd0548fad1976fb1eea9265a33
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-06-20 13:36:38 -0500

    images uploaded and loops created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
-var win;
-var loss;
+var win=0;
+var loss=0;
 var random_result;
+var previous = 0;
 
-random_number = Math.floor(Math.random()*69) +30;
+var resetAndStart = function (){
+  $(".crystals").empty();
+  var images=[
+  'crystal1.jpg',
+  'crystal2.jpg',
+  'crystal3.jpg',
+  'crystal4.jpg']
+random_result = Math.floor(Math.random()*69) +30;
 $("#result").html("Random Result:" + random_result);
 
 for(var i=0; i< 4; i++){
@@ -11,11 +19,34 @@ console.log(random);
   var crystal = $("<div>");
       crystal.attr({"class": "crystal", "data-random":random
       });
-
+crystal.css({
+  "background-images":"url('"+ images[i]+"')",
+  "background-size":"cover"
+})
       $(".crystals").append(crystal);
 }
+$("previous").html("Total Score:" +previous);
+}
 
-$(".crystal").on('click', function(){
-  console.log($(this).attr("data-random")
-  
+resetAndStart ();
+$(document).on('click',".crystal", function(){
+  var num = prseInt($(this).attr('data-random'));
+  previous += num;
+
+  $("previous").html("Total Score:" +previous);
+
+  if(previous> random_result){
+loss--;
+$("#loss").html("You lost:",loss);
+
+previous=0;
+resetAndStart ();
+  }
+  else if(previous = random_result){
+win++;
+$("#win").html("You win:",win);
+previous=0;
+
+resetAndStart ();
+  }
 });
